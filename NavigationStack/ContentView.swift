20d85961f5ac0534dd3c1a109a721c55923b4c58
Lisandro Falconi <lfalconi.consultant@newscorp.com>
2022-11-28 14:16:25 -0300

    chore: Initial setup of a NavigationStack with link

--- NavigationStack/ContentView.swift
+++ NavigationStack/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var platforms: [Platform] = [.init(name: "Xbox", imageName: "xbox.logo", color: .green),
+                                 .init(name: "Playstation", imageName: "playstation.logo", color: .indigo),
+                                 .init(name: "PC", imageName: "pc", color: .pink),
+                                 .init(name: "Mobile", imageName: "iphone", color: .mint),]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section("Platforms") {
+                    ForEach(platforms, id: \.name) { platform in
+                        NavigationLink(value: platform) {
+                            Label(platform.name, systemImage: platform.imageName)
+                                .foregroundColor(platform.color)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
@@ -23,4 +34,9 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Platform: Hashable {
+    let name, imageName: String
+    let color: Color
 }
