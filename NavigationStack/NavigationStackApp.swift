4a4947f74a5655865e813fb3f27370d060c3a699
Lisandro Falconi <lfalconi.consultant@newscorp.com>
2022-11-28 14:06:15 -0300

    Initial Commit

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Lisandro Falconi on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
